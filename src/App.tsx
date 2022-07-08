02ae382b59c1c1589debe870a3b008056c5cd91e
Ayomidemi <93412345+Ayomidemi@users.noreply.github.com>
2022-07-08 06:18:58 +0100

    create note form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
+import CreateNote from './components/CreateNote';
 import Header from './components/Header';
 import NoteList from './components/NoteList';
 import { Note } from './models/model';
@@ -19,11 +20,19 @@ function App() {
     <div className="App">
       <Header />
        <Container className='mt-5'>
+
         <Row>
           <Col>
           <NoteList notes={notes} setNotes={setNotes} />
           </Col>
         </Row>
+
+        <Row>
+          <Col>
+            <CreateNote notes={notes} setNotes={ setNotes}/>
+          </Col>
+        </Row>
+
        </Container>
     </div>
   );
--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import * as React from "react";
+import { Alert, Button, Form } from "react-bootstrap";
+import { Note } from "../models/model";
 
-const CreateNote = () => {
-  return (
-    <div>CreateNote</div>
-  )
+interface ICreateNoteProps {
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
 }
 
-export default CreateNote
+const CreateNote: React.FunctionComponent<ICreateNoteProps> = ({
+  notes,
+  setNotes,
+}) => {
+
+  const [error, setError] = React.useState<string>('')
+
+  const titleRef = React.useRef<HTMLInputElement | null>(null);
+  const textRef = React.useRef<HTMLTextAreaElement | null>(null);
+  const colorRef = React.useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    
+    if (titleRef.current?.value === "" || textRef.current?.value === "") {
+      return setError("Please fill all inputs");
+  }
+
+  setError("");
+        setNotes([...notes, {
+            id: (new Date()).toString(),
+            title: (titleRef.current as HTMLInputElement).value,
+            text: (textRef.current as HTMLTextAreaElement).value,
+            color: (colorRef.current as HTMLInputElement).value,
+            date: (new Date()).toString()
+        }]);
+        
+        (titleRef.current as HTMLInputElement).value = "";
+        (textRef.current as HTMLTextAreaElement).value = "";
+  }
+
+  return (
+    <>
+      <h2 className="mt-3">Create Notes</h2>
+      {error && <Alert variant='danger'>{error}</Alert>}
+
+      <Form className="mb-3 mt-3" onSubmit={(e) => handleSubmit(e)}>
+        <Form.Group className="mb-3" controlId="formBasicTitle">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Title e.g Meetings"
+            ref={titleRef}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Text</Form.Label>
+          <Form.Control
+            placeholder="Note descrption"
+            as="textarea"
+            ref={textRef}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label htmlFor="colorInput">Add a color</Form.Label>
+          <Form.Control
+            type="color"
+            id="colorInput"
+            defaultValue="#dfdfdf"
+            title="Choose your color"
+            ref={colorRef}
+          />
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default CreateNote;
